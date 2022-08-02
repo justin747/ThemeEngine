92ebfb3778b58c29c5067242b662863c00418dca
justin747 <justin747@gmail.com>
2022-08-01 17:42:03 -0700

    Initial Commit
    Creating Theme Engine package for import into apps

--- Sources/ThemeEngine/ThemeEngine.swift
+++ Sources/ThemeEngine/ThemeEngine.swift
@@ -1,6 +1,21 @@
-public struct ThemeEngine {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import SwiftUI
 
-    public init() {
-    }
+protocol Theme {
+    @available(macOS 10.15, *)
+    var primaryColor:    Color { get }
+    
+    @available(macOS 10.15, *)
+    var secondaryColor:  Color { get }
+    
+    @available(macOS 10.15, *)
+    var tertiaryColor:   Color { get }
+    
+    @available(macOS 10.15, *)
+    var quaternaryColor: Color { get }
+    
+    @available(macOS 10.15, *)
+    var quinaryColor:    Color { get }
+    
+    var themeName:       String { get }
 }
--- Tests/ThemeEngineTests/ThemeEngineTests.swift
+++ Tests/ThemeEngineTests/ThemeEngineTests.swift
@@ -6,6 +6,5 @@ final class ThemeEngineTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ThemeEngine().text, "Hello, World!")
     }
 }
